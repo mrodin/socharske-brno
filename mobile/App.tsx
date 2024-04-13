965c5dd8b26d7d89b8345c13c9bcde3c51415d07
Petr Pololáník <petr.pololanik@pekatvision.com>
2024-04-13 19:09:36 +0200

    feat: :zap: Add auto search, improve structure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,11 @@
-import { StatusBar } from "expo-status-bar"
-import { Button, StyleSheet, Text, View } from "react-native"
-import { useState } from "react"
-import { Map } from "./app/screens/Map"
-import { User } from "./app/screens/User"
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Main } from "./app/screens/Main";
+import { User } from "./app/screens/User";
 
 export default function App() {
-  const [screen, setScreen] = useState("home")
-
-  if (screen === "home") {
-    return (
-      <View style={styles.container}>
-        <Text>Sochařské brno app!</Text>
-        <StatusBar style="auto" />
-        <Button title="Map" onPress={() => setScreen("map")} />
-        <Button title="User" onPress={() => setScreen("user")} />
-      </View>
-    )
-  } else if (screen === "map") {
-    return <Map />
-  } else if (screen === "user") {
-    return <User />
-  }
+  return <Main />;
 }
 
 const styles = StyleSheet.create({
@@ -30,4 +15,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-})
+});
---
+++ mobile/app/screens/Main.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Button } from "react-native-elements";
+import { Map } from "../components/Map";
+import { CollectionButton } from "../components/CollectionButton";
+import { MenuButton } from "../components/MenuButton";
+
+export const Main = () => {
+  const [showLeftDrawer, setShowLeftDrawer] = React.useState(false);
+
+  return (
+    <>
+      <Map />
+      <View style={styles.leftDrawerButton}>
+        <MenuButton onPress={() => setShowLeftDrawer(true)} />
+      </View>
+      <View style={styles.collectionButton}>
+        <CollectionButton onPress={() => {}} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  collectionButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  leftDrawerButton: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+  },
+});
