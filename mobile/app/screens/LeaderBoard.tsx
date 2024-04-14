2bdbb5c8df09fbdb2f866182048cd35d73fe5eee
Martin Rodin <rodin.martin@gmail.com>
2024-04-14 08:24:24 +0200

    basic leaderboards

--- mobile/app/screens/LeaderBoard.tsx
+++ mobile/app/screens/LeaderBoard.tsx
@@ -4,11 +4,20 @@ import { useLeaderBoard } from "../api/statues";
 import { BackToMapButton } from "../components/BackToMapButton";
 import { UserTag } from "../components/UserTag";
 import { Title } from "../components/Title";
+import { Winner } from "../components/Winner";
+
+const AdamImage = require("../../assets/images/adam.jpeg");
+const KubaImage = require("../../assets/images/kuba.jpeg");
+const PepeImage = require("../../assets/images/pepe.jpeg");
+
+const images: any = {
+  "117faf40-2317-42f7-a8a7-680b7a37c1b7": PepeImage,
+  "11fc039a-1999-48c3-bc8b-2683b1eb4cdb": KubaImage,
+  "40379104-5e6a-4b79-a17b-54da5fd3d2a7": AdamImage,
+};
 
 export function LeaderBoard({ onClose }: { onClose: () => void }) {
   const users = useLeaderBoard();
-
-  console.log(users);
 
   return (
     <SafeAreaView>
@@ -22,14 +31,18 @@ export function LeaderBoard({ onClose }: { onClose: () => void }) {
         </View>
       </View>
       <Text>Leader board</Text>
-      {users.map((user) => (
-        <View key={user.id}>
-          <Text>
-            {user.username} - {user.score}
-          </Text>
-        </View>
-      ))}
-      <Button onPress={onClose} title="Zavřít" />
+      {users.map((user) => {
+        return (
+          <>
+            <Winner
+              key={user.id}
+              name={user.username}
+              score={user.score.toFixed()}
+              thumbnail={images[user.id]}
+            />
+          </>
+        );
+      })}
     </SafeAreaView>
   );
 }
