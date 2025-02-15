f89a5a3fa484b0a8b0eb412448bb2b8f26e97558
Martin Rodin <rodin.martin@gmail.com>
2025-02-15 15:16:55 +0100

    feat: leaderboard redesign

--- mobile/app/(tabs)/leaderboard.tsx
+++ mobile/app/(tabs)/leaderboard.tsx
@@ -1,11 +1,10 @@
 import { FC } from "react";
-import { View, SafeAreaView, StyleSheet } from "react-native";
+import { View, SafeAreaView, ScrollView } from "react-native";
 
 import { useGetLeaderboard } from "@/api/queries";
-import { BackToMapButton } from "@/components/BackToMapButton";
+import { RouteHeader } from "@/components/RouteHeader";
 import { Player } from "@/components/Player";
 import { Title } from "@/components/Title";
-import { UserTag } from "@/components/UserTag";
 import { Winner } from "@/components/Winner";
 
 const AdamImage = require("../../assets/images/adam.jpeg");
@@ -18,61 +17,45 @@ const images: any = {
   "40379104-5e6a-4b79-a17b-54da5fd3d2a7": AdamImage,
 };
 
-type LeaderBoardProps = {
-  onClose: () => void;
-};
-
-const LeaderBoard: FC<LeaderBoardProps> = ({ onClose }) => {
+const LeaderBoard: FC = () => {
   const { data: leaderboard } = useGetLeaderboard();
 
   return (
-    <SafeAreaView>
-      <View style={{ gap: 30 }}>
-        <View style={styles.row}>
-          <BackToMapButton onClose={onClose} />
-          <UserTag />
-        </View>
-        <View style={styles.row}>
-          <Title>Nejlepší lovci soch</Title>
-        </View>
-        <View style={styles.entries}>
-          {leaderboard.map((user, index) => {
-            if (index === 0) {
+    <SafeAreaView className="flex-1">
+      <ScrollView className="flex-1" contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="bg-gray flex-1 gap-8 px-6">
+          <RouteHeader route="Leaderboard" />
+
+          <Title className="text-gray-pale text-[20px] tracking-wide">
+            Nejlepší lovci soch
+          </Title>
+
+          <View className="gap-4">
+            {leaderboard.map((user, index) => {
+              if (index === 0) {
+                return (
+                  <Winner
+                    key={user.id}
+                    name={user.username}
+                    score={user.score.toFixed()}
+                    thumbnail={images[user.id]}
+                  />
+                );
+              }
               return (
-                <Winner
+                <Player
                   key={user.id}
                   name={user.username}
                   score={user.score.toFixed()}
                   thumbnail={images[user.id]}
                 />
               );
-            }
-            return (
-              <Player
-                key={user.id}
-                name={user.username}
-                score={user.score.toFixed()}
-                thumbnail={images[user.id]}
-              />
-            );
-          })}
+            })}
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  entries: {
-    paddingHorizontal: 24,
-    gap: 16,
-  },
-  row: {
-    paddingHorizontal: 24,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-});
 
 export default LeaderBoard;
