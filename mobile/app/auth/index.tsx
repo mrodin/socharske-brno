94b6c2e33cd56ef8fcd94b8d68134a699c84fcab
Petr Pololáník <petr.pololanik@pekatvision.com>
2025-03-23 14:09:05 +0100

    Add Apple auth

--- mobile/app/auth/index.tsx
+++ mobile/app/auth/index.tsx
@@ -7,8 +7,9 @@ import { GoogleIcon } from "@/icons/GoogleIcon";
 import { MailIcon } from "@/icons/MailIcon";
 import { googleAuth } from "@/utils/googleAuth";
 import AuthWrap from "@/components/auth/Wrap";
-import { Link, useRouter } from "expo-router";
-import * as AppleAuthentication from "expo-apple-authentication";
+import { useRouter } from "expo-router";
+
+import { appleAuth } from "@/utils/appleAuth";
 
 const Auth = () => {
   const router = useRouter();
@@ -20,6 +21,11 @@ const Auth = () => {
       </Text>
       <View className="gap-4">
         <Button
+          onPress={appleAuth}
+          icon={<AppleIcon className="top-[2px]" />}
+          title="Přihlásit se přes Apple"
+        />
+        <Button
           onPress={googleAuth}
           icon={<GoogleIcon className="top-[2px]" />}
           title="Přihlásit se přes Google"
@@ -28,34 +34,6 @@ const Auth = () => {
           onPress={() => router.push("/auth/email-signin")}
           icon={<MailIcon className="top" />}
           title="Přihlásit se přes Email"
-        />
-      </View>
-      <View>
-        <AppleAuthentication.AppleAuthenticationButton
-          buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-          buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
-          cornerRadius={5}
-          style={{
-            width: 200,
-            height: 44,
-          }}
-          onPress={async () => {
-            try {
-              const credential = await AppleAuthentication.signInAsync({
-                requestedScopes: [
-                  AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                  AppleAuthentication.AppleAuthenticationScope.EMAIL,
-                ],
-              });
-              // signed in
-            } catch (e) {
-              if (e.code === "ERR_REQUEST_CANCELED") {
-                // handle that the user canceled the sign-in flow
-              } else {
-                // handle other errors
-              }
-            }
-          }}
         />
       </View>
       <Text className="text-white text-xl text-center mb-4 mt-10 font-krona">
---
+++ mobile/src/utils/appleAuth.tsx
@@ -0,0 +1,39 @@
+import * as AppleAuthentication from "expo-apple-authentication";
+import { supabase } from "./supabase";
+import { Alert } from "react-native";
+
+export const appleAuth = async () => {
+  try {
+    const credential = await AppleAuthentication.signInAsync({
+      requestedScopes: [
+        AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+        AppleAuthentication.AppleAuthenticationScope.EMAIL,
+      ],
+    });
+    // Sign in via Supabase Auth.
+    if (credential.identityToken) {
+      const {
+        error,
+        data: { user },
+      } = await supabase.auth.signInWithIdToken({
+        provider: "apple",
+        token: credential.identityToken,
+      });
+      if (!error) {
+        // User is signed in.
+        console.log("User signed in", user);
+      }
+    } else {
+      throw new Error("No identityToken.");
+    }
+  } catch (e) {
+    if (e.code === "ERR_REQUEST_CANCELED") {
+      // handle that the user canceled the sign-in flow
+      console.log("User canceled the sign-in flow");
+    } else {
+      // handle other errors
+      console.error("Error during Apple sign-in", e);
+      Alert.alert("Neco se pokazilo");
+    }
+  }
+};
