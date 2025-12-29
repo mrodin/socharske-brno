372c6f28cbb59609afae3c0f868fa564d002ee56
Martin Rodin <rodin.martin@gmail.com>
2025-12-29 17:53:27 +0100

    fix: add early return to UserTag

--- mobile/src/components/UserTag.tsx
+++ mobile/src/components/UserTag.tsx
@@ -2,23 +2,24 @@ import { Image, View, Text } from "react-native";
 import { useContext } from "react";
 import { UserAvatarContext } from "../providers/UserAvatar";
 import { UserInfoContext } from "../providers/UserInfo";
-import { useGetLeaderboard } from "@/api/queries";
 import { useUserStatistics } from "@/hooks/useUserStatistics";
 import { defaultUserIconSource } from "@/utils/images";
 
 export const UserTag = () => {
-  const { url } = useContext(UserAvatarContext);
+  const { url: avatarUrl } = useContext(UserAvatarContext);
   const { userInfo } = useContext(UserInfoContext);
 
   const userStatistics = useUserStatistics();
+
+  if (!userInfo || !userStatistics) return null;
 
   return (
     <View className="flex-row justify-between items-center border-[2px] w-full border-gray-light rounded-full gap-2.5">
       <Image
         source={
-          url
+          avatarUrl
             ? {
-                uri: url,
+                uri: avatarUrl,
               }
             : defaultUserIconSource
         }
@@ -30,13 +31,11 @@ export const UserTag = () => {
           ellipsizeMode="tail"
           numberOfLines={1}
         >
-          {userInfo?.username}
+          {userInfo.username}
         </Text>
-        <Text className="color-red-pale">{userStatistics?.rank}. místo</Text>
+        <Text className="color-red-pale">{userStatistics.rank}. místo</Text>
       </View>
-      <Text className="color-white text-2xl pr-6">
-        {userStatistics?.score}b
-      </Text>
+      <Text className="color-white text-2xl pr-6">{userStatistics.score}b</Text>
     </View>
   );
 };
